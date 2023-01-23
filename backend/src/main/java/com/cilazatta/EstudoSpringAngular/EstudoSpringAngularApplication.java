86d8215a8726a33f334e832a63b437b7522b2793
wzatta <wzjssa@gmail.com>
2023-01-23 16:40:10 -0300

    Criação dos Projetos Spring (Backend) e Angular (Frontend)

---
+++ backend/src/main/java/com/cilazatta/EstudoSpringAngular/EstudoSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.cilazatta.EstudoSpringAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpringAngularApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cilazatta/EstudoApringAngular/EstudoApringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cilazatta.EstudoApringAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoApringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
